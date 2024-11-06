23a1fb2233459c4e9c6121898ce5fe99d7a60d8c
Federico Dionisi <code@fdionisi.me>
2024-11-06 16:26:34 +0100

    refactor: expose library from AnthropicServer

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,33 @@
-mod cli;
-mod client;
-mod provider;
-mod routes;
-mod server_state;
-mod usage_reporter;
-
+use anthropic_server::{provider::Provider, AnthropicServer};
 use anyhow::Result;
 use clap::Parser;
+use tokio::net::ToSocketAddrs;
 use tracing_subscriber::{layer::SubscriberExt, util::SubscriberInitExt};
+
+/// Anthropic proxy server.
+///
+/// The server allows to communicate with various Anthropic providers, maintaining a single API for all of them.
+#[derive(clap::Parser)]
+pub struct Cli {
+    /// The token used for authenticating all incoming requests
+    #[clap(long, env = "AUTH_TOKEN")]
+    pub api_key: String,
+    /// The host to bind to
+    #[clap(long, default_value = "0.0.0.0")]
+    host: String,
+    /// The port to bind to
+    #[clap(long, default_value = "3000")]
+    port: u16,
+    /// The provider to use for the model
+    #[command(subcommand)]
+    pub provider: Provider,
+}
+
+impl Cli {
+    pub fn address(&self) -> impl ToSocketAddrs {
+        (self.host.to_owned(), self.port)
+    }
+}
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -20,13 +40,14 @@ async fn main() -> Result<()> {
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let cli = cli::Cli::parse();
+    let cli = Cli::parse();
+    let address = cli.address();
 
-    let server = cli.http().await?;
-    let listener = tokio::net::TcpListener::bind(cli.address()).await?;
-
-    tracing::debug!("listening on {}", listener.local_addr()?);
-    axum::serve(listener, server).await?;
-
-    Ok(())
+    AnthropicServer::builder()
+        .with_provider(cli.provider)
+        .with_api_key(cli.api_key)
+        .build()
+        .await?
+        .serve(address)
+        .await
 }
